b49df61f8c9772f2aa28883a8a9de37957c72160
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-12 11:55:59 +0200

    Tested css-modules.

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.test}>
       <p>Hur ser det ut?</p>
     </div>
   );
