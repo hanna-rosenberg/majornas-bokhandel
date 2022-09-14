a0ffc4a262771cf0fe466b8ee85708c7455e99db
itisalbin <to.albin@gmail.com>
2022-09-14 15:06:32 +0200

    Navbar and Welcome underway.

--- web/components/Navbar/Navbar.js
+++ web/components/Navbar/Navbar.js
@@ -2,8 +2,22 @@ import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <>
-      <p className={styles.test}>Navbar</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.leftMarginContainer}></div>
+      <div className={styles.middleContainer}>
+        <div className={styles.desktopLinksContainer}>
+          <a href="#" className={styles.desktopLink}>
+            <span>Hem</span>
+          </a>
+          <a href="#" className={styles.desktopLink}>
+            <span>Om Oss</span>
+          </a>
+          <a href="#" className={styles.desktopLink}>
+            <span>Evenemang</span>
+          </a>
+        </div>
+      </div>
+      <div className={styles.rightMarginContainer}></div>
+    </div>
   );
 }
--- web/components/Welcome/Welcome.js
+++ web/components/Welcome/Welcome.js
@@ -2,8 +2,16 @@ import styles from "./Welcome.module.css";
 
 export default function Welcome() {
   return (
-    <>
-      <p className={styles.test}>Welcome</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.marginTopLeft}></div>
+      <div className={styles.marginMiddleLeft}></div>
+      <div className={styles.marginBottomLeft}></div>
+      <div className={styles.empty}></div>
+      <div className={styles.empty}></div>
+      <div className={styles.empty}></div>
+      <div className={styles.marginTopRight}></div>
+      <div className={styles.marginMiddleRight}></div>
+      <div className={styles.marginBottomRight}></div>
+    </div>
   );
 }
