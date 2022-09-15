5d2d3c804f22b0b69f20470403a98ed994ee7ca5
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-09-15 16:58:05 +0200

    NewsAndOffers - component

--- web/components/NewsAndOffers/NewsAndOffers.jsx
+++ web/components/NewsAndOffers/NewsAndOffers.jsx
@@ -1,6 +1,7 @@
 import styles from "./NewsAndOffers.module.css";
+import React from "react";
 
-export default function NewsAndOffers() {
+export default function NewsAndOffers({ news, offer }) {
   return (
     <div className={styles.container}>
       <div className={styles.dividerTopLeft}></div>
@@ -8,10 +9,16 @@ export default function NewsAndOffers() {
       <div className={styles.dividerTopRight}></div>
 
       <div className={styles.newsLeft}></div>
-      <div className={styles.news}></div>
+      <div className={styles.news}>
+        <h3>NYHETER</h3>
+        <p>{news}</p>
+      </div>
       <div className={styles.newsOffersDivider}></div>
       <div className={styles.offersLeftMobile}></div>
-      <div className={styles.offers}></div>
+      <div className={styles.offers}>
+        <h3>ERBJUDANDE</h3>
+        <p>{offer}</p>
+      </div>
       <div className={styles.offersRight}></div>
 
       <div className={styles.mobileDividerLeft}></div>
