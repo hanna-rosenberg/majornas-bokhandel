0dd54580ec72ebc6aa3c6dacadc5ceaf412958d7
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-15 12:05:43 +0200

    Added AboutEvents grid.

--- web/components/AboutEvents/AboutEvents.js
+++
@@ -1,9 +0,0 @@
-import styles from "./AboutEvents.module.css";
-
-export default function AboutEvents() {
-  return (
-    <>
-      <p className={styles.test}>AboutEvents</p>
-    </>
-  );
-}
---
+++ web/components/AboutEvents/AboutEvents.jsx
@@ -0,0 +1,13 @@
+import styles from "./AboutEvents.module.css";
+
+export default function AboutEvents() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.dividerTopLeft}></div>
+      <div className={styles.dividerLeft}></div>
+      <div className={styles.textContainer}></div>
+      <div className={styles.dividerTop}></div>
+      <div className={styles.dividerRight}></div>
+    </div>
+  );
+}
