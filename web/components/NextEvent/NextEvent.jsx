c03dc1322f9d79d38f2095e6d01751f6bf662003
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-15 14:22:35 +0200

    Added NextEvent grid.

--- web/components/NextEvent/NextEvent.jsx
+++ web/components/NextEvent/NextEvent.jsx
@@ -2,8 +2,17 @@ import styles from "./NextEvent.module.css";
 
 export default function NextEvent() {
   return (
-    <>
-      <p className={styles.test}>NextEvent</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.top}></div>
+      <div className={styles.left}></div>
+      <div className={styles.right}></div>
+      <div className={styles.infoContainer}></div>
+      <div className={styles.middleDivider}></div>
+      <div className={styles.imgContainer}></div>
+      <div className={styles.mobileTopLeft}></div>
+      <div className={styles.mobileTopRight}></div>
+      <div className={styles.mobileRight}></div>
+      <div className={styles.mobileDividerRight}></div>
+    </div>
   );
 }
