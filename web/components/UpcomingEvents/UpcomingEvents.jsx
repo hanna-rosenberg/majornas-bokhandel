18b0ba17a6f95075e5bdc9f4e317bb8ae3dba8ae
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-18 12:00:15 +0200

    UpcomingEvents desktop grid.

--- web/components/UpcomingEvents/UpcomingEvents.jsx
+++ web/components/UpcomingEvents/UpcomingEvents.jsx
@@ -2,8 +2,20 @@ import styles from "./UpcomingEvents.module.css";
 
 export default function UpcommingEvents() {
   return (
-    <>
-      <p className={styles.test}>UpcommingEvents</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.left}></div>
+      <div className={styles.top}></div>
+      <div className={styles.topRight}></div>
+      <div className={styles.right}></div>
+      <div className={styles.information}></div>
+      <div className={styles.upcoming}></div>
+      <div className={styles.bottomLeft}></div>
+      <div className={styles.bottomRight}></div>
+      <div className={styles.mobileTopLeft}></div>
+      <div className={styles.mobileTop}></div>
+      <div className={styles.mobileTopRight}></div>
+      <div className={styles.mobileMiddleDivider}></div>
+      <div className={styles.mobileLeft}></div>
+    </div>
   );
 }
