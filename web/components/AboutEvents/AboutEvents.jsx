1838fc5061810c0923cf3e9f6cb74d1b657beb65
sowulff <sowu99@gmail.com>
2022-09-20 11:24:14 +0200

    about-event component✅

--- web/components/AboutEvents/AboutEvents.jsx
+++ web/components/AboutEvents/AboutEvents.jsx
@@ -1,11 +1,19 @@
 import styles from "./AboutEvents.module.css";
+import Image from "next/image";
+import bookIcon from "../../assets/book-icon.svg";
 
 export default function AboutEvents() {
   return (
     <div className={styles.container}>
       <div className={styles.dividerTopLeft}></div>
       <div className={styles.dividerLeft}></div>
-      <div className={styles.textContainer}></div>
+      <div className={styles.textContainer}>
+        <Image src={bookIcon} className={styles.bookIcon}></Image>
+        <h4>
+          Bokhandeln anordnar regelbundet författarkvällar. En rad
+          uppmärksammade och intressanta författare har gästat bokhandeln.
+        </h4>
+      </div>
       <div className={styles.dividerTop}></div>
       <div className={styles.dividerRight}></div>
     </div>
