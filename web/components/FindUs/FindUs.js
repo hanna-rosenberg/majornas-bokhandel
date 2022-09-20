0baf431a96a06a31c010ce11ac615be59952dafc
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-20 09:59:57 +0200

    FindUs grid done.

--- web/components/FindUs/FindUs.js
+++
@@ -1,9 +0,0 @@
-import styles from "./FindUs.module.css";
-
-export default function FindUs() {
-  return (
-    <>
-      <p className={styles.test}>FindUs</p>
-    </>
-  );
-}
---
+++ web/components/FindUs/FindUs.jsx
@@ -0,0 +1,23 @@
+import styles from "./FindUs.module.css";
+
+export default function FindUs() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.left}></div>
+        <div className={styles.right}></div>
+        <div className={styles.bottom}></div>
+        <div className={styles.info}></div>
+        <div className={styles.map}></div>
+        <div className={styles.mobileMiddleEdgeLeft}></div>
+        <div className={styles.mobileMiddleEdgeRight}></div>
+        <div className={styles.mobileMiddleLeft}></div>
+        <div className={styles.mobileMiddleRight}></div>
+        <div className={styles.mobileLeft}></div>
+        <div className={styles.mobileRight}></div>
+        <div className={styles.mobileBottomLeft}></div>
+        <div className={styles.mobileBottomRight}></div>
+      </div>
+    </>
+  );
+}
