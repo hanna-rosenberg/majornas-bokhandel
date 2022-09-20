cc986841587f746f421af9ee09ba2a5af28e7f7a
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-09-20 14:13:52 +0200

    nextEvent-componant

--- web/components/NextEvent/NextEvent.jsx
+++ web/components/NextEvent/NextEvent.jsx
@@ -1,5 +1,6 @@
 import styles from "./NextEvent.module.css";
 import React from "react";
+import { urlFor } from "../../../studio/lib/client";
 
 export default function NextEvent({ events }) {
   return (
@@ -7,13 +8,15 @@ export default function NextEvent({ events }) {
       <div className={styles.top}></div>
       <div className={styles.left}></div>
       <div className={styles.right}></div>
-      <div className={styles.infoContainer}></div>
+      <div className={styles.infoContainer}>
+        <img src={urlFor(events.image).url()} />
+      </div>
       <div className={styles.middleDivider}></div>
       <div className={styles.imgContainer}>
         <h2>{events.name}</h2>
         <h4>{events.booktitle}</h4>
-        <p>{events.date}</p>
-        <p>{events.description}</p>
+        <p className={styles.eventDate}>{events.date}</p>
+        <p className={styles.eventDescription}>{events.description}</p>
       </div>
       <div className={styles.mobileTopLeft}></div>
       <div className={styles.mobileTopRight}></div>
