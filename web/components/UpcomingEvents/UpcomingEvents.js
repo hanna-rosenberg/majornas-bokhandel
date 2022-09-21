cc853c4e5648dd19f8bb979bc390440ef8121b6c
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-09-21 12:32:19 +0200

    moved key to parent div

--- web/components/UpcomingEvents/UpcomingEvents.js
+++ web/components/UpcomingEvents/UpcomingEvents.js
@@ -29,22 +29,16 @@ export default function UpcommingEvents({ upcoming }) {
           <h2>Kommande evenemang</h2>
           <ul>
             {upcoming.slice(0, 3).map(function (event) {
-              console.log(event._id);
+              // console.log(event._id);
               return (
-                <div className={styles.upcomingEvents}>
+                <div className={styles.upcomingEvents} key={event._id}>
                   <div className={styles.upcomingEventsImg}>
-                    <img key={event._id} src={urlFor(event.image).url()} />
+                    <img src={urlFor(event.image).url()} />
                   </div>
                   <div className={styles.upcomingEventsText}>
-                    <li className={styles.type} key={event._id}>
-                      {event.type}
-                    </li>
-                    <li className={styles.name} key={event._id}>
-                      {event.name}
-                    </li>
-                    <li className={styles.date} key={event._id}>
-                      {event.date}
-                    </li>
+                    <li className={styles.type}>{event.type}</li>
+                    <li className={styles.name}>{event.name}</li>
+                    <li className={styles.date}>{event.date}</li>
                   </div>
                 </div>
               );
