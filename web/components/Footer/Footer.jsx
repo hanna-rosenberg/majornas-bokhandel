4cc51a9200e939dead71b3837028dad713009195
Sakariash <simonsakarias@gmail.com>
2022-09-22 23:59:35 +0200

    messed up the footer, lets try again

--- web/components/Footer/Footer.jsx
+++ web/components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import styles from "./Footer.module.css";
 
-export default function Footer(props) {
+export default function Footer({ color, info }) {
   return (
     <div className={styles.container}>
-      <div className={styles.left} style={{ backgroundColor: props.color }}></div>
-      <div className={styles.info}>Footer Info</div>
+      <div className={styles.left} style={{ backgroundColor: color }}></div>
+      <div className={styles.info}>
+        {" "}
+        <div className={styles.infoContainer}>
+          <h4>{info.address}.</h4>
+          <h4>
+            {info.openingHoursHeading}: {info.openingHours},{" "}
+            {info.openingHoursWeekend}
+          </h4>
+          <h4>Telefon: {info.phoneNumber}</h4>
+          <h4>{info.email}</h4>
+        </div>
+      </div>
       <div className={styles.right}></div>
     </div>
   );
