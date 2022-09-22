495d9dceedfffec487968606a99450c4ee3f64f6
sowulff <sowu99@gmail.com>
2022-09-22 15:20:12 +0200

    header upcoming events

--- web/components/EventsPageComponents/EventsUpcomingEvents/EventsUpcomingEvents.jsx
+++ web/components/EventsPageComponents/EventsUpcomingEvents/EventsUpcomingEvents.jsx
@@ -3,7 +3,9 @@ export default function Welcome() {
   return (
     <div className={styles.container}>
       <div className={styles.topLeft}></div>
-      <div className={styles.header}></div>
+      <div className={styles.header}>
+        <h2>Kommande evenemang</h2>
+      </div>
       <div className={styles.topRight}></div>
       <div className={styles.left}></div>
       <div className={styles.events}></div>
