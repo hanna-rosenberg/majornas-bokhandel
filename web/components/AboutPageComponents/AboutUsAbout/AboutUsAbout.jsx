8e26d7e9949c59f3fc304d8c9b3d805b3cc8cf68
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-26 12:42:37 +0200

    AboutUsAbout grid done.

---
+++ web/components/AboutPageComponents/AboutUsAbout/AboutUsAbout.jsx
@@ -0,0 +1,19 @@
+import styles from "./AboutUsAbout.module.css";
+export default function AboutUsAbout() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.topLeft}></div>
+      <div className={styles.AboutUs}></div>
+      <div className={styles.topRight}></div>
+      <div className={styles.topLeftLower}></div>
+      <div className={styles.left}></div>
+      <div className={styles.topDivider}></div>
+      <div className={styles.right}></div>
+      <div className={styles.image}></div>
+      <div className={styles.middleDivider}></div>
+      <div className={styles.infoOwner}></div>
+      <div className={styles.bottomLeft}></div>
+      <div className={styles.bottom}></div>
+    </div>
+  );
+}
--- web/pages/about/index.js
+++ web/pages/about/index.js
@@ -1,11 +1,13 @@
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import { client } from "../../../studio/lib/client";
+import AboutUsAbout from "../../components/AboutPageComponents/AboutUsAbout/AboutUsAbout";
 
 export default function Home({ infoData }) {
   return (
     <>
       <Navbar />
+      <AboutUsAbout />
       <Footer color="#edba88" info={infoData[0]} />
     </>
   );
