2b22dad615be43910a8037f084d50e6d79173fec
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-09-26 10:44:33 +0200

    Instagram grid done.

--- web/components/Instagram/Instagram.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Instagram.module.css";
-
-export default function Instagram() {
-  return (
-    <>
-      <p className={styles.test}>Instagram</p>
-    </>
-  );
-}
---
+++ web/components/Instagram/Instagram.jsx
@@ -0,0 +1,14 @@
+import styles from "./Instagram.module.css";
+
+export default function Instagram() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.topLeft}></div>
+      <div className={styles.header}></div>
+      <div className={styles.topRight}></div>
+      <div className={styles.left}></div>
+      <div className={styles.content}></div>
+      <div className={styles.right}></div>
+    </div>
+  );
+}
