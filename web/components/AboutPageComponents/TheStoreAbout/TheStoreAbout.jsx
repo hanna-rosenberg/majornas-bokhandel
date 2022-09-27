bd32e9969422da016aedd867cf23003b506d0d40
sowulff <sowu99@gmail.com>
2022-09-27 11:11:26 +0200

    fixed the images with a gap in the grid

--- web/components/AboutPageComponents/TheStoreAbout/TheStoreAbout.jsx
+++ web/components/AboutPageComponents/TheStoreAbout/TheStoreAbout.jsx
@@ -1,16 +1,25 @@
 import styles from "./TheStoreAbout.module.css";
+import Image from "next/image";
+import Image0ne from "../../../assets/store-one.jpg";
+import ImageTwo from "../../../assets/store-two.jpg";
 export default function TheStoreAbout() {
   return (
     <div className={styles.container}>
       <div className={styles.topLeft}></div>
-      <div className={styles.header}></div>
+      <div className={styles.header}>
+        <h2>Butiken</h2>
+      </div>
       <div className={styles.topRight}></div>
       <div className={styles.left}></div>
       <div className={styles.topDivider}></div>
       <div className={styles.right}></div>
-      <div className={styles.image}></div>
+      <div className={styles.image}>
+        <Image src={Image0ne}></Image>
+      </div>
       <div className={styles.middleDivider}></div>
-      <div className={styles.imageTwo}></div>
+      <div className={styles.imageTwo}>
+        <Image src={ImageTwo}></Image>
+      </div>
       <div className={styles.bottom}></div>
       <div className={styles.mobileMiddleDivider}></div>
     </div>
